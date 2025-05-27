42f6b3e5ae20c4337285fef38deb1a67231b1b16
Sanna-Ali <133492348+Sanna-Ali@users.noreply.github.com>
2025-05-27 22:03:01 +0300

    Update and rename likes.js to likes.route.js

--- routes/likes.route.js
+++ routes/likes.route.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const connection = require("../config/DB");
-const { addLike, getLike, disLike } = require("../controller/likes");
+const { addLike, getLike, disLike } = require("../controller/likes.controller.js");
 router.get("/", getLike);
 router.post("/add", addLike);
 router.put("/:id", disLike);
