7a52325619e44cb6542cd408117dafc5de407410
Sanna-Ali <133492348+Sanna-Ali@users.noreply.github.com>
2025-05-27 22:04:04 +0300

    Update and rename post.js to post.route.js

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const connection = require("../config/DB");
-const { addPost, getPosts } = require("../controller/post");
+const { addPost, getPosts } = require("../controller/post.controller.js
+");
 router.get("/", getPosts);
 router.post("/add", addPost);
 module.exports = router;
